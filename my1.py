fcfadf6dfcdfe08b05ea3f3ddd560c03503c5348
Mathivanan S <smathivananmathi62@gmail.com>
2026-01-24 16:46:43 +0530

    Update my1.py

--- my1.py
+++ my1.py
@@ -1,1 +1,13 @@
+pipeline {
+    agent any // Runs on any available agent
+    stages {
+        stage('Check Python Version') {
+            steps {
+                echo 'Checking Python version...'
+                // Executes the 'python3 --version' command in the shell
+                sh 'python3 --version'
+            }
+        }
+    }
+}
 
